5add319bbebb969e4d78572c4e5e8e7923b07422
Randy Hanak <rhanak@gmail.com>
2013-03-04 00:07:57 -0500

    making front flips baby

--- keep/playing/Readme.txt
+++ keep/playing/Readme.txt
@@ -4,9 +4,8 @@ The Car driving game should play a boom sound when the car hits any of the walls
 The NPC is the sprite bottle moving across the screen, this is just to show basic movement with a non player character.
 
 To run this type:
-python pygameprimer.py 
+python skydivemain.py 
 
 References:
 
-"boom.wav" taken from pygame examples in the data directory.
-"Cars movie image" retains its copyright with respect to Disney. Taken from http://community.flixster.com/quizzes/disney-pixar-cars-movie-characters
+"Mount everest image" retains its copyright with respect to hdwallpaperpics. Taken from http://cdn.hdwallpaperspics.com/uploads/2013/01/mount_everest_wallpapers_2-normal.jpg
--- keep/playing/util.py
+++ keep/playing/util.py
@@ -1,16 +1,39 @@
 import os
 import pygame
 
+from pygame.locals import *
+from pygame.compat import geterror
+    
 main_dir = os.path.split(os.path.abspath(__file__))[0]
+data_dir = os.path.join(main_dir, 'data')
 
-def load_image(file):
-    "loads an image, prepares it for play"
-    file = os.path.join(main_dir, 'data', file)
+#functions to create our resources
+def load_image(name, colorkey=None):
+    fullname = os.path.join(data_dir, name)
     try:
-        surface = pygame.image.load(file)
+        image = pygame.image.load(fullname)
     except pygame.error:
-        raise SystemExit('Could not load image "%s" %s'%(file, pygame.get_error()))
-    return surface.convert()
+        print ('Cannot load image:', fullname)
+        raise SystemExit(str(geterror()))
+    image = image.convert()
+    if colorkey is not None:
+        if colorkey is -1:
+            colorkey = image.get_at((0,0))
+        image.set_colorkey(colorkey, RLEACCEL)
+    return image, image.get_rect()
+
+def load_sound(name):
+    class NoneSound:
+        def play(self): pass
+    if not pygame.mixer or not pygame.mixer.get_init():
+        return NoneSound()
+    fullname = os.path.join(data_dir, name)
+    try:
+        sound = pygame.mixer.Sound(fullname)
+    except pygame.error:
+        print ('Cannot load sound: %s' % fullname)
+        raise SystemExit(str(geterror()))
+    return sound
 
 def load_images(*files):
     imgs = []
@@ -18,15 +41,22 @@ def load_images(*files):
         imgs.append(load_image(file))
     return imgs
 
-class dummysound:
-    def play(self): pass
-
-def load_sound(file):
-    if not pygame.mixer: return dummysound()
-    file = os.path.join(main_dir, 'data', file)
-    try:
-        sound = pygame.mixer.Sound(file)
-        return sound
-    except pygame.error:
-        print ('Warning, unable to load, %s' % file)
-    return dummysound()
+def load_sliced_sprites(w, h, filename):
+    '''
+    Specs :
+      Master can be any height.
+      Sprites frames width must be the same width
+      Master width must be len(frames)*frame.width
+    Assuming you ressources directory is named "ressources"
+    '''
+    images = []
+    master_image = pygame.image.load(os.path.join(data_dir, filename)).convert_alpha()
+    
+    master_width, master_height = master_image.get_size()
+    for i in xrange(int(master_width/w)):
+      # set the trasparent color
+      image = master_image.subsurface((i*w,0,w,h))
+      colorkey = image.get_at((2,2))
+      image.set_colorkey(colorkey)
+      images.append(image)
+    return images
